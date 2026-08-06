9c6820bf3918f770fbc1e01bfe86ac463bc9fa9f
PMTraders-24 <powermbatha1@gmail.com>
2026-08-06 19:01:30 +0200

    Update products.js

--- products.js
+++ products.js
@@ -1,2 +1,37 @@
-const PRODUCTS=[{id:1,name:"8 inch SDD weave Human Hair",price:850,category:"Weave",icon:"8 inch weave.jpeg " },{id:2,name:"iPhone 13 Pro Max",price:7500,category:"Cellphones",icon:"
-  iPhone 13 Pro Max.jpeg"},{id:3,name:"Fashion Dress",price:230,category:"Ladies Clothing",icon:"👗"},{id:4,name:"20 Inch Weave Jurly Curls",price:970,category:"Weave",icon:"💇🏾‍♀️"},{id:5,name:"iPhone 17 Pro Max",price:24000,category:"Cellphones",icon:"📱"}];
+const PRODUCTS = [
+  {
+    id: 1,
+    name: "8 inch SDD weave Human Hair",
+    price: 850,
+    category: "Weave",
+    image: "8 inch SDD Weave"
+  },
+  {
+    id: 2,
+    name: "iPhone 13 Pro Max",
+    price: 7500,
+    category: "Cellphones",
+    image: "iPhone 13 Pro Max.jpeg"
+  },
+  {
+    id: 3,
+    name: "Fashion Dress",
+    price: 230,
+    category: "Ladies Clothing",
+    image: "Classic Ladies Dress.jpg"
+  },
+  {
+    id: 4,
+    name: "20 Inch Weave Jurly Curls",
+    price: 970,
+    category: "Weave",
+    image: "8 inch SDD pixey curly.jpg"
+  },
+  {
+    id: 5,
+    name: "iPhone 17 Pro Max",
+    price: 24000,
+    category: "Cellphones",
+    image: "iPhone 17 Pro Max.webp"
+  }
+];
